61d5a058829a420cc88499359dc99c6bf82d0317
lakshaygarg <lakshaygarg@users.noreply.github.com>
2023-02-04 10:25:41 -0500

    Preliminary market data update

--- Configuration.py
+++ Configuration.py
@@ -1,5 +1,15 @@
 # Module to keep constants and hard-coded values
 # as well as paths and security names
 
-DATE_COL = 'date'
+DATE_COL = 'Date'
 DATE_FORMAT = r'%Y-%m-%d'
+
+import os
+cwd = os.getcwd()
+
+DATA_PATH = cwd + '\DataFiles'
+INDEX_DATA = DATA_PATH + '\index_prices.csv'
+
+INDEX_LIST = ['^GSPC', '^IXIC', '^DJI','^RUA']
+
+FUNC_BIND = {'index' : 'read_index_data'}
--- MarketUtils.py
+++ MarketUtils.py
@@ -1,6 +1,13 @@
 #------ Market Data Functions ------#
 import pandas as pd
 from Configuration import *
+import yfinance as yf
+from datetime import datetime, timedelta
+
+#------- download index data ------#
+def download_index_data():
+    prices = yf.download(INDEX_LIST).dropna()['Adj Close']
+    prices.to_csv(INDEX_DATA)
 
 #------ Reads data from csv file with ------#
 def read_raw_data(filename, **kwargs):
@@ -8,15 +15,36 @@ def read_raw_data(filename, **kwargs):
     raw_data[DATE_COL] = pd.to_datetime(raw_data[DATE_COL], format=DATE_FORMAT)
     return raw_data
 
-#------ Get data for a few dates -------#
-def get_data_for_interval(filename,termDates,**kwargs):
-    raw_data = read_raw_data(filename, **kwargs)
-    mask = (raw_data[DATE_COL] > termDates[0] and raw_data[DATE_COL] < termDates[1])
-    return raw_data.loc[mask]
+#----- read index data -------#
+def read_index_data(**kwargs):
+    raw_prices = read_raw_data(INDEX_DATA, **kwargs)
+    return raw_prices
 
+#------ Get data -------#
+def get_data(**kwargs):
+
+    col_list = ["Date"]
+
+    if 'type' in kwargs:
+       type = kwargs['type']
+    else :
+       type = "index"
+
+    if type == 'index':
+       if 'list' in kwargs:
+           col_list += kwargs['list']
+       else:
+           col_list += INDEX_LIST
+
+    data_reader = globals()[FUNC_BIND[type]]
+    raw_data = data_reader(**kwargs)[col_list]
+
+    if 'termDates' in kwargs:
+        termDates = kwargs['termDates']
+        mask = (raw_data[DATE_COL] > termDates[0] and raw_data[DATE_COL] < termDates[1])
+        return raw_data.loc[mask]
+    
+    return raw_data
 
 #------ Data cleaning tasks --------#
 def clean_raw_data(data): pass
-
-
-
