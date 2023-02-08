6fdda696e54f8ec5b11438674e63571c0285c261
lulu643 <70429879+lulu643@users.noreply.github.com>
2023-02-08 10:22:29 -0500

    Update AnalyticsEngine.py

--- AnalyticsEngine.py
+++ AnalyticsEngine.py
@@ -4,8 +4,8 @@ import pyfolio as pf
 def analytics(rets, benchmark_rets):
     """
     Generate a number of tear sheets that are useful for analyzing a strategy’s performance
-    :param rets: Daily returns of the strategy, noncumulative
-    :param benchmark_rets: Daily noncumulative returns of the benchmark
+    :param rets: daily returns of the strategy, noncumulative
+    :param benchmark_rets: daily returns of the benchmark, noncumulative
     :return: None
     """
     pf.create_full_tear_sheet(returns=rets, benchmark_rets=benchmark_rets)
